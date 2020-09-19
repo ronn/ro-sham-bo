4b5b897ecbf372b57acdb03d974f93d11261a563
ronn <ronnie@nextdigital.es>
2020-09-19 17:51:11 +0200

    Improve gameLogic

--- src/main/java/com/ronn/roshambo/service/GameService.java
+++ src/main/java/com/ronn/roshambo/service/GameService.java
@@ -1,24 +1,62 @@
 package com.ronn.roshambo.service;
 
+import com.ronn.roshambo.dto.ResultDTO;
 import com.ronn.roshambo.model.*;
+import com.ronn.roshambo.model.shape.Rock;
+import com.ronn.roshambo.repository.GameRepository;
+import com.ronn.roshambo.web.GameRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static com.ronn.roshambo.model.RoundResult.*;
 
+@Service
 public class GameService {
 
   Game game = new Game();
 
-  public Game playRound(Round round) {
-    round.setResult(defineResult(round.getPlayer1(), round.getPlayer2()));
+  @Autowired
+  GameRepository resultRepository;
+
+  public Game playRound() {
+    Shape chosenShape1 = Shape.getRandomShape();
+    Shape chosenShape2 = new Rock();
+
+    Player player1 = new Player(chosenShape1.getName());
+    Player player2 = new Player(chosenShape2.getName());
+
+    RoundResult roundResult = defineResult(chosenShape1, chosenShape2);
+
+    Round round = new Round(player1, player2, roundResult);
+
+    resultRepository.save(new ResultDTO(roundResult.name()));
     game.addRound(round);
 
     return game;
   }
 
-  public RoundResult defineResult(Player p1, Player p2) {
-    Shape chosenShape1 = p1.getChosenShape();
-    Shape chosenShape2 = p2.getChosenShape();
+  public GameRecord getResults(){
 
+    List<ResultDTO> all = resultRepository.findAll();
+
+    long draws = all.stream()
+        .filter(r -> DRAW.name().equals(r.getResult()))
+        .count();
+
+    long p1Wins = all.stream()
+        .filter(r -> PLAYER_1.name().equals(r.getResult()))
+        .count();
+
+    long p2Wins = all.stream()
+        .filter(r -> PLAYER_2.name().equals(r.getResult()))
+        .count();
+
+    return new GameRecord(all.size(), p1Wins, p2Wins, draws);
+  }
+
+  public RoundResult defineResult(Shape chosenShape1, Shape chosenShape2) {
     if (chosenShape1.getClass().equals(chosenShape2.getClass())) {
       return DRAW;
     }
