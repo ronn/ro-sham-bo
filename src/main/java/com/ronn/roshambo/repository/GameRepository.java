a1a3834638192b230519280a51b5a17ac89b32f0
ronn <ronnie@nextdigital.es>
2020-09-19 17:50:48 +0200

    Create repo

--- src/main/java/com/ronn/roshambo/repository/GameRepository.java
+++ src/main/java/com/ronn/roshambo/repository/GameRepository.java
@@ -1,2 +1,9 @@
-package com.ronn.roshambo.repository;public class GameRepository {
+package com.ronn.roshambo.repository;
+
+import com.ronn.roshambo.dto.ResultDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<ResultDTO, Long> {
 }
