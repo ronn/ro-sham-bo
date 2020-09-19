6b5e902e32ef3fa7a44bf257ad94bc82c5e1d879
ronn <ronnie@nextdigital.es>
2020-09-19 17:49:31 +0200

    Refactor behavior

--- src/test/java/com/ronn/roshambo/service/GameServiceTest.java
+++ src/test/java/com/ronn/roshambo/service/GameServiceTest.java
@@ -1,11 +1,12 @@
 package com.ronn.roshambo.service;
 
-import com.ronn.roshambo.model.*;
+import com.ronn.roshambo.model.Game;
+import com.ronn.roshambo.model.RoundResult;
+import com.ronn.roshambo.model.shape.Rock;
 import org.junit.jupiter.api.Test;
 
 import static com.ronn.roshambo.model.RoundResult.DRAW;
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameServiceTest {
 
@@ -24,7 +25,7 @@ class GameServiceTest {
   void when_same_shape_is_chosen_then_result_is_draw() {
     GameService gameService = new GameService();
 
-    RoundResult roundResult = gameService.defineResult(new Player(PlayingType.ALWAYS_ROCK), new Player(PlayingType.ALWAYS_ROCK));
+    RoundResult roundResult = gameService.defineResult(new Rock(), new Rock());
     assertEquals(DRAW, roundResult);
   }
 }
