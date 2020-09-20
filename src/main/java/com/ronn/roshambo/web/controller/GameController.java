53e4404929282c1d9730e8f7b9d7b7d522f1a4dd
ronn <ronnie@nextdigital.es>
2020-09-20 21:28:36 +0200

    First version on api controller

--- src/main/java/com/ronn/roshambo/web/controller/GameController.java
+++ src/main/java/com/ronn/roshambo/web/controller/GameController.java
@@ -1,18 +1,18 @@
 package com.ronn.roshambo.web.controller;
 
-import com.ronn.roshambo.dto.ResultDTO;
 import com.ronn.roshambo.model.Game;
 import com.ronn.roshambo.service.GameService;
 import com.ronn.roshambo.web.GameRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/game")
 public class GameController {
 
   @Autowired
@@ -26,8 +26,14 @@ public class GameController {
     return new ResponseEntity<>(game, HttpStatus.OK);
   }
 
-  @GetMapping("/games")
+  @GetMapping("/results")
   public ResponseEntity<GameRecord> getGamesTotal(){
      return new ResponseEntity<>(gameService.getResults(), HttpStatus.OK);
+  }
+
+  //@DeleteMapping()
+  @GetMapping("/game/del")
+  public ResponseEntity<Game> restartGame(){
+    return new ResponseEntity<>(gameService.restartGame(), HttpStatus.OK);
   }
 }
